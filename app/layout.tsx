0306c5dde4dc6952d5938dd26505a674436e29db
zhanghao835 <zhanghao835@shizhuang-inc.com>
2024-01-16 20:29:58 +0800

    feat: add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
 import RootNavbar from '@/components/RootNavbar';
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <body className={inter.className}>
           <RootNavbar />
           {children}
+          <Analytics />
         </body>
       </html>
     </ClerkProvider>
